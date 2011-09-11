b21af40e15e17b13e8f4d195ef9fbe5772895008
Naveen Garg <naveen.garg@gmail.com>
2011-09-11 11:02:19 -0500

    use wxpython window instead of ipython

--- pyexample.py
+++ pyexample.py
@@ -1,4 +1,4 @@
-          
+# if you don't want to use wxpython then do the next 2 lines          
 # python /c/Python26/Scripts/ipython.py -wthread -i pyquery.py
 # must use -wthread otherwise calling com client hangs
 
@@ -6,6 +6,8 @@
 import win32com.client
 import time, datetime
 from pcomserver import PComServer
+import wx
+
 class PyExample(PComServer):
     def printMatrix(self, matrix):
         print type(matrix)
@@ -21,4 +23,8 @@ class PyExample(PComServer):
 clsid = "{55C2F76F-5136-4614-A397-12214CC011E5}"
 appid = "Python.Example"
 p = PyExample(clsid, appid)
+app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
+frame = wx.Frame(None, wx.ID_ANY, "py com example") # A Frame is a top-level window.
+frame.Show(True)     # Show the frame.
+app.MainLoop()
     
